97c173a704d19ee6ce0370d2945e7c60283430f4
Joride <kerrelinc@gmail.com>
2021-03-02 20:53:48 +0100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Jorrit van Asselt on 02/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MonarchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonarchApp.swift
+//  Shared
+//
+//  Created by Jorrit van Asselt on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct MonarchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
